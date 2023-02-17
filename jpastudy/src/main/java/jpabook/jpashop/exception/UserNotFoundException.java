3c198c5c0f8b78785a6f2db92622008aee5da672
Ssantoo <nowing0108@kakao.com>
2023-02-17 13:10:41 +0900

    feat: exception

---
+++ jpastudy/src/main/java/jpabook/jpashop/exception/UserNotFoundException.java
@@ -0,0 +1,8 @@
+package jpabook.jpashop.exception;
+
+public class UserNotFoundException extends RuntimeException {
+
+    public UserNotFoundException(String message){
+        super(message);
+    }
+}
