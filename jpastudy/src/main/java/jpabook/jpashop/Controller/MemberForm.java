7c145ed17c02d3e18b3303612a09e24b2db7ad51
DESKTOP-RM1C3C1\82107 <nowing0108@kakao.com>
2023-02-26 18:47:39 +0900

    회원등록

---
+++ jpastudy/src/main/java/jpabook/jpashop/Controller/MemberForm.java
@@ -0,0 +1,18 @@
+package jpabook.jpashop.Controller;
+
+import lombok.Getter;
+import lombok.Setter;
+import javax.validation.constraints.NotEmpty;
+
+
+@Getter
+@Setter
+public class MemberForm {
+
+    @NotEmpty(message ="")
+    private String name;
+
+    private String city;
+    private String street;
+    private String zipcode;
+}
