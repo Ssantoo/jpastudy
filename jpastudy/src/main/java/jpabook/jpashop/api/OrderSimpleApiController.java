ba69cb79dfc59505611c1773da892078b4475cfc
DESKTOP-RM1C3C1\82107 <nowing0108@kakao.com>
2023-03-02 12:40:37 +0900

    V2 : 엔티티를 DTO로 변환

--- jpastudy/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ jpastudy/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -1,13 +1,18 @@
 package jpabook.jpashop.api;
 
+import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 
@@ -42,6 +47,40 @@ public class OrderSimpleApiController {
 
     }
 
+
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDto> ordersV2(){
+
+
+        //결과 2개 -> 쿼릭가 총 5번 나간다 Order / member /delivery  / 또다른 결과의 member delivery
+        // 1 + N(2)  -> 1 + 회원N + 배송 N
+        //특히 eagger로 하면 더 심각하다
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+
+        return orders.stream()
+                .map(o -> new SimpleOrderDto(o))
+                .collect(Collectors.toList());
+
+    }
+
+    @Data
+    static class SimpleOrderDto{
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrderDto(Order order){
+            orderId = order.getId();
+            name = order.getMember().getName();
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress();
+
+        }
+
+    }
 
 
 
